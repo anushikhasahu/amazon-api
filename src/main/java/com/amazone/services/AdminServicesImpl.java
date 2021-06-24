f003f3c738c6a18c5d396c65b32bdbc9f14e214f
anu <anushikhasahu@gmail.com>
2021-06-23 23:30:16 -0400

    changes

--- src/main/java/com/amazone/services/AdminServicesImpl.java
+++ src/main/java/com/amazone/services/AdminServicesImpl.java
@@ -1,5 +1,6 @@
 package com.amazone.services;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.amazone.exception.UserNotFoundException;
@@ -8,14 +9,15 @@ import com.amazone.repository.AdminRepository;
 
 @Service
 public class AdminServicesImpl implements AdminServices {
-
+	@Autowired
+	AdminRepository adminDAO;
 	@Override
 	public boolean validateAdmin(String username, String password) throws UserNotFoundException {
-		return false;
-//		boolean result = adminDAO.existsByAdminIdAndPassword(username, password);
-//		if(!result)
-//			throw new UserNotFoundException("User Not Found");
-//		return result;
+	//	return false;
+		boolean result = adminDAO.existsByAdminIdAndPassword(username, password);
+		if(!result)
+			throw new UserNotFoundException("User Not Found");
+		return result;
 	}
 
 	@Override
